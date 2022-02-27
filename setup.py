5e30fe6e60285fcec602e05e2edff913c1ab4abd
Garvit-25 <56580370+Garvit-25@users.noreply.github.com>
2022-02-27 17:45:30 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'Topsis-Garvit-101903778',         
+  packages = ['Topsis-Garvit-101903778'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'A Python package to find TOPSIS for multi-criteria decision analysis method',   # Give a short description about your library
+  author = 'Garvit Nangia',                   
+  author_email = 'nagiagarvit@live.com',      
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['TOPSIS','TIET'],   
+  install_requires=[            
+          'pandas',
+          'tabulate',
+          'sys',
+          'math',
+          'os',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
